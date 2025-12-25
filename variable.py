50fc5a61aaec38259a5f9005ade7558f79f5ec1e
smmazharul <smmazharulislam20@gmail.com>
2025-12-25 10:38:00 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,13 @@
+price = 10
+rating = 4.9
+name = "Mazharul"
+is_publish = True #Flase
+print(price)
+
+name = "john Smith"
+age = 20
+is_new = True
+
+temperature = 32
+full_name = "Rafi"
+is_learnig_python = True
