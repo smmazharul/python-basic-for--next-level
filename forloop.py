9f4440baa1abb566c6671cdf4cc093ed8ac1270a
smmazharul <smmazharulislam20@gmail.com>
2025-12-27 19:27:28 +0600

    tuple

--- forloop.py
+++ forloop.py
@@ -52,9 +52,40 @@ print(f'The sum of all numbers divisible by 3 or 5 between 1 and 50 is: {result}
 
 """
 # the reverse string
+"""
 names = ['john', 'sarah', 'kelly','jams','bob','jenifer','zara']
 # for name in reversed(names):
 for name in names[::-1]:
    if name[0]=="j" or name[0]=="J":
        print(name)
        break
+       
+"""
+# the max finder 
+"""
+numbers = [3,5,2,8,6,4,10,12,7]
+max_number = numbers[0]
+for number in numbers:
+    if number >max_number:
+        max_number = number
+print(f'The maximum number in the list is: {max_number}')
+"""
+# 2d list 
+"""
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+total = 0
+for row in matrix[1:2]:
+    for item in row:
+        print(item)
+"""
+nums = [1,2,3,4,5]
+nums.append(6)
+nums.insert(1,10)
+nums.remove(3)
+nums.pop()
+nums.sort()
+nums.reverse()
---
+++ tuple.py
@@ -0,0 +1,5 @@
+
+# numbers = (1, 2, 3, 4, 5)
+numbers = [9,10,11,12,1,2,3,4,5,6,7,8]
+
+print(len(numbers))
