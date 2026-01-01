406dd532e355dc22cb6421677ad5558872a5803a
smmazharul <smmazharulislam20@gmail.com>
2026-01-01 17:32:46 +0600

    recap what I learn

---
+++ recap4.py
@@ -0,0 +1,41 @@
+# my_list = list()
+# nums = [1,2,3,3,5,4,5,5,6,7,8,9]
+# mixed = [1, "Hello", 3.14, True]
+# print(mixed)
+
+# fruits = ["banana","appple","date","cherry"]
+# # positive indexing 
+# print(fruits[0])
+# print(fruits[2])
+
+# # Nagetive indexing 
+# print(fruits[-1])
+# print(fruits[-2])
+
+# stack = ['python', 'Java']
+# print(stack)
+# stack.append('C++')
+# stack.append('Javascript')
+# print(stack)
+# stack.insert(1,"C#")
+# print(stack)
+# extra = ["Ruby","Golang"]
+# stack.extend(extra)
+# print(stack)
+
+
+# import time 
+
+# def measure_performance():
+#     list_A = [1,2,3,4,5,6,4,5,8,6,11,2,1,54,4,44,4,4,54,54,4,54,54,54,5,4641146,4,4,3,84,641,64,9,4]
+#     for i in range(10000000):
+#         list_A.append(i)
+#     start_time = time.time()
+    
+#     list_A.pop(0)
+#     end_time = time.time()
+#     print(f"Time to insert: {end_time - start_time:.5f} secounds ")
+
+# measure_performance()
+
+
